27f0613505b7e48387a3d8084d638f3156d7fad1
Henry Raether <henry.raether@gmail.com>
2021-02-03 10:15:33 -0600

    HW4 V2

--- noober.js
+++ noober.js
@@ -23,7 +23,39 @@ async function pageLoaded() {
           levelOfService = 'Noober X'
         } 
       } 
-      if (ride.length == 1){ 
+      if (ride[0].purpleRequested){
+        let outputElement = document.querySelector('.rides')
+      outputElement.insertAdjacentHTML('beforeend',`<h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+            <i class="fas fa-car-side"></i>
+            <span>${levelOfService}</span> 
+          </h1>
+          <div class="border-4 border-purple-500 p-4 my-4 text-left"> 
+            <div class="flex">
+              <div class="w-1/2">
+                <h2 class="text-2xl py-1">${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}</h2>
+                <p class="font-bold text-gray-600">${ride[0].passengerDetails.phoneNumber}</p>
+              </div>
+              <div class="w-1/2 text-right">
+                <span class="rounded-xl bg-gray-600 text-white p-2">
+                ${ride[0].numberOfPassengers} passengers
+                </span>
+              </div>
+            </div>
+            <div class="mt-4 flex">
+              <div class="w-1/2">
+                <div class="text-sm font-bold text-gray-600">PICKUP</div>
+                <p>${ride[0].pickupLocation.address}</p>
+                <p>${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}</p>
+              </div>
+              <div class="w-1/2">
+                <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+                <p>${ride[0].dropoffLocation.address}</p>
+                <p>${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}</p>
+              </div>
+            </div>
+          </div>`)
+      }
+      else if (ride.length == 1){ 
       let outputElement = document.querySelector('.rides')
       outputElement.insertAdjacentHTML('beforeend',`<h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
             <i class="fas fa-car-side"></i>
